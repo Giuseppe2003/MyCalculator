4672cff5ae4b36ec39230a783acd4af3bbbd608d
Giuseppe <g.petti4@studenti.unimol.it>
2026-01-06 16:38:45 +0100

    prova checkstyle
    è stata volutamente introdotto un errore di stile per verificare che checkstyle funzioni.

--- src/main/java/org/example/operations/Addition.java
+++ src/main/java/org/example/operations/Addition.java
@@ -2,7 +2,7 @@ package org.example.operations;
 
 public class Addition{
 
-    public static double add(double a, double b) {
+    public static double add    (double a, double b) {
         return a + b;
     }
 }
